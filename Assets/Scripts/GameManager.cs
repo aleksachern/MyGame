f20eee254f3bce0d671a917f512da8f56613f2a0
aleksachern <aleksa_chern@mail.ru>
2021-05-26 20:50:01 +0300

    First commit.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+public class GameManager : MonoBehaviour
+{
+    public CarController RR;
+         
+    public GameObject needle;
+    private float startPosition = 220f, endPosition = -49f;
+    private float desiredPosition;
+
+    public float vehicleSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        vehicleSpeed = RR.CurrentSpeed;
+        updateNeedle();
+    }
+
+    public void updateNeedle()
+    {
+        desiredPosition = startPosition - endPosition;
+        float temp = vehicleSpeed / 180;
+        needle.transform.eulerAngles = new Vector3(0, 0, (startPosition - temp * desiredPosition));
+
+    } 
+
+}
